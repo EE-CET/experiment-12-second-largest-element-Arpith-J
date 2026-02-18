a595dbd985ac3c6c1d19fdbfb80905743cd855df
Arpith Joseph <arpithjoseph2005@gmail.com>
2026-02-18 16:42:27 +0530

    Duplicate condition checked

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -12,14 +12,14 @@ public class SecondLargest {
         }
 
         public static void secondLargest(int[] arr){
-                int large1=arr[0];
-                int large2=arr[0];
-                for(int i=0; i<arr.length; i++){
+                int large1=Integer.MIN_VALUE;
+                int large2=Integer.MIN_VALUE;
+                for(int i=0; i<arr.length; i    ++){
                         if(arr[i] > large1){
                                 large2 = large1;
                                 large1 = arr[i];
                         }
-                        else if(arr[i] > large2){
+                        else if(arr[i] > large2 && arr[i] != large1){
                                 large2 = arr[i];
                         }
                 }
